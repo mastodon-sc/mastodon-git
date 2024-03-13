1b415993ac7c8dd8ccb101b9151f58b4e73a69f4
Matthias Arzt <arzt@mpi-cbg.de>
2024-03-13 11:04:17 +0100

    Change the MastodonGitSetAutorCommand into a modal dialog
    This allows to show the dialog before another command, like commit or
    sync is executed.
    
    (Executing a scijava command and waiting for it to finish is not possible
    from the AWT thread.)

---
+++ src/main/java/org/mastodon/mamut/collaboration/commands/SetAuthorDialog.java
@@ -0,0 +1,62 @@
+package org.mastodon.mamut.collaboration.commands;
+
+import java.util.concurrent.CancellationException;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
+
+import org.mastodon.mamut.collaboration.settings.DefaultMastodonGitSettingsService;
+import org.mastodon.mamut.collaboration.settings.MastodonGitSettingsService;
+import org.scijava.Context;
+import org.scijava.prefs.PrefService;
+
+public class SetAuthorDialog
+{
+	private SetAuthorDialog()
+	{
+		// prevent from instantiation
+	}
+
+	/**
+	 * Shows a dialog that asks the user for an author name and email address.
+	 * Saves the name and email in the MastodonGitSettingsService.
+	 *
+	 * @throws CancellationException if the user presses cancel.
+	 */
+	public static void setAuthorName( MastodonGitSettingsService settings )
+	{
+		final String description = "<html><body align=left>"
+				+ "The name and email that you specify below<br>"
+				+ "are used to identify you as the author of the<br>"
+				+ "changes you make to the shared project.<br><br>"
+				+ "Name and email are likely to become publicly visible on the internet.<br>"
+				+ "You may use a nickname and dummy email address if you wish.<br><br>";
+		JPanel panel = new JPanel();
+		panel.setLayout( new MigLayout( "insets dialog" ) );
+		panel.add( new JLabel( description ), "span, grow, wrap" );
+		panel.add( new JLabel( "Author Name" ), "align right" );
+		JTextField authorField = new JTextField();
+		panel.add( authorField, "grow, wrap" );
+		panel.add( new JLabel( "Author Email" ), "align right" );
+		JTextField emailField = new JTextField( "noreply@example.com" );
+		panel.add( emailField, "grow, wrap" );
+		int result = JOptionPane.showOptionDialog( null, panel, "Set Author Name", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new Object[] { "Set Name", "Cancel" }, null );
+		if ( result == 0 )
+		{
+			settings.setAuthorName( authorField.getText() );
+			settings.setAuthorEmail( emailField.getText() );
+		}
+	}
+
+	public static void main( String... args )
+	{
+		Context context = new Context();
+		PrefService settings = context.getService( PrefService.class );
+		settings.remove( DefaultMastodonGitSettingsService.class, "author.name" );
+		settings.remove( DefaultMastodonGitSettingsService.class, "author.email" );
+	}
+}

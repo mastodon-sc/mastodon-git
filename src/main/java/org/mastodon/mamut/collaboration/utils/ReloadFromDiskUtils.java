61e9e4e50d553f25e70184e075d976383d2bda41
Matthias Arzt <arzt@mpi-cbg.de>
2024-03-20 12:14:44 +0100

    ReloadFromDiskUtils: update javadoc, make constructor private

--- src/main/java/org/mastodon/mamut/collaboration/utils/ReloadFromDiskUtils.java
+++ src/main/java/org/mastodon/mamut/collaboration/utils/ReloadFromDiskUtils.java
@@ -13,12 +13,16 @@ import org.mastodon.model.AbstractModelImporter;
  */
 public class ReloadFromDiskUtils
 {
+	private ReloadFromDiskUtils()
+	{
+		// Prevent instantiation of utility class.
+	}
 
 	/**
 	 * Reloads the model from disk.
-	 * <p>
-	 * It does so by opening the Mastodon project again, and copying all the
-	 * spots and links from the new model to the existing one.
+	 * <br>
+	 * This clears the undo history and reloads the ModelGraph and tags.
+	 * and tags. It does not reload the feature model, project XML or dataset XML.
 	 */
 	public static void reloadFromDisk( ProjectModel projectModel ) throws IOException
 	{

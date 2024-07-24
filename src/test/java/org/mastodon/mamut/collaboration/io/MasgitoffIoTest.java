f1ae7ada088d3a18f3dbad1d685503464d6f73f9
Matthias Arzt <arzt@mpi-cbg.de>
2024-07-24 11:16:50 +0200

    Masgitoff: write and read spot labels

--- src/test/java/org/mastodon/mamut/collaboration/io/MasgitoffIoTest.java
+++ src/test/java/org/mastodon/mamut/collaboration/io/MasgitoffIoTest.java
@@ -2,6 +2,7 @@ package org.mastodon.mamut.collaboration.io;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
@@ -9,6 +10,7 @@ import org.mastodon.mamut.ProjectModel;
 import org.mastodon.mamut.collaboration.utils.ModelAsserts;
 import org.mastodon.mamut.io.ProjectLoader;
 import org.mastodon.mamut.model.ModelGraph;
+import org.mastodon.mamut.model.Spot;
 import org.scijava.Context;
 
 import mpicbg.spim.data.SpimDataException;
@@ -23,6 +25,9 @@ public class MasgitoffIoTest
 		{
 			final ProjectModel projectModel = ProjectLoader.open( "/home/arzt/devel/mastodon/mastodon-git/src/test/resources/org/mastodon/mamut/collaboration/tiny/tiny-project.mastodon", context );
 			final ModelGraph graph = projectModel.getModel().getGraph();
+			Iterator< Spot > iterator = graph.vertices().iterator();
+			iterator.next().setLabel( "some spot" );
+			iterator.next().setLabel( "some other spot" );
 			final File file = new File( "tmp.masgitoff" );
 			if ( file.isDirectory() )
 				FileUtils.deleteDirectory( file );

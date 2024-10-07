0de78d738a5e54094cae376095ce486359e4cd5b
Matthias Arzt <arzt@mpi-cbg.de>
2024-10-07 11:36:52 +0200

    Fix ModelAssertsTest

--- src/test/java/org/mastodon/mamut/collaboration/utils/ModelAssertsTest.java
+++ src/test/java/org/mastodon/mamut/collaboration/utils/ModelAssertsTest.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -125,7 +125,7 @@ public class ModelAssertsTest
 				+ "== Tag set: index=1 name=tabset2 (no tags)\n"
 				+ "Spot(label=A, timepoint=0, position=[0.0, 0.0, 0.0], cov=[[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]): 0=tag1\n"
 				+ "Spot(label=B, timepoint=1, position=[0.0, 0.0, 0.0], cov=[[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]): 0=tag2\n"
-				+ "Spot(label=A, timepoint=0, position=[0.0, 0.0, 0.0], cov=[[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])->Spot(label=B, timepoint=1, position=[0.0, 0.0, 0.0], cov=[[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]):Spot(label=A, timepoint=0, position=[0.0, 0.0, 0.0], cov=[[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])->Spot(label=B, timepoint=1, position=[0.0, 0.0, 0.0], cov=[[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]): 0=tag1";
+				+ "Spot(label=A, timepoint=0, position=[0.0, 0.0, 0.0], cov=[[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])->Spot(label=B, timepoint=1, position=[0.0, 0.0, 0.0], cov=[[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]): 0=tag1\n";
 		assertEquals( expected, text );
 	}
 
